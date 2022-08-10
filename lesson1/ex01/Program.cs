e841d21f64d2715c512f5fa6c059eb28718e609d
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-08-10 14:11:45 +0300

    ex01 done

--- lesson1/ex01/Program.cs
+++ lesson1/ex01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = b;
+if (max < min)
+{
+    max = b;
+    min = a;   
+}
+
+Console.WriteLine($"большее число: {max} меньшее число: {min} ");
