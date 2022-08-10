8a2a12e2c771200a46d3cf22580f0b77278d9835
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-08-10 15:06:05 +0300

    ex02,03,04 done

--- lesson1/ex04/Program.cs
+++ lesson1/ex04/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int value = 1;
+while (value <= N){
+    if(value % 2 == 0){
+        Console.Write(value + " ");
+    }
+    value++;
+}
+Console.WriteLine();
