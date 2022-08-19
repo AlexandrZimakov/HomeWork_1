0a22305e742e75b92998aade1288d9169221cbd1
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-08-19 16:09:41 +0300

    ex3

--- lesson3/ex01/Program.cs
+++ lesson3/ex01/Program.cs
@@ -1,12 +1,21 @@
-﻿Console.Write("Введите шестизначное число: ");
+﻿bool isPolindrom(int number) {
+    int[] array = new int[6];
+    for (int i = 0; i < 6; i++){
+        array[i] = number % 10;
+        number = number / 10;
+    }
+    if (   array[0] == array[5] 
+        && array[1] == array[4] 
+        && array[2] == array[3]
+        )
+        return true;
+        else return false;
+}
+
+Console.Write("Введите шестизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int[6];
-for (int i = 0; i < 6; i++){
-    array[i] = number % 10;
-    number = number / 10;
-}
-if (array[0]==array[5] && array[1]==array[4] && array[2]==array[3]){
+if (isPolindrom(number)) {
     Console.WriteLine($"Ваше число - палиндром.");
 }
 else {
