5e99caab9a719ac6c45444c2197d5e323124f2fb
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-09-15 16:58:33 +0300

    HW4

---
+++ lesson4/ex02/Program.cs
@@ -0,0 +1,13 @@
+﻿int GetSum(int number){
+    int sumNumber = 0;
+
+    while (number > 0){
+        sumNumber += number % 10;
+        number /= 10;
+    }
+    return sumNumber;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetSum(number));
