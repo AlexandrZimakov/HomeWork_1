099665780b6ae0eb1b146a25178f8e7da33b26ec
AlexandrZimakov <42317697+AlexandrZimakov@users.noreply.github.com>
2022-09-16 12:46:36 +0300

    HW4

--- lesson4/ex03/Program.cs
+++ lesson4/ex03/Program.cs
@@ -1,3 +1,27 @@
-﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-int[] numbers = new int[8];
+﻿void AllArray(int[] massiv){
+    int index = 0;
+    while (index < massiv.Length){
+        massiv[index] = new Random().Next(0,10);
+        index++;
+    }
+}
 
+void ArrayWrite(int[] massiv){
+    int index = 0;
+    Console.Write("[ ");
+    while (index < massiv.Length){
+        Console.Write($"{massiv[index]}");
+        index++;
+        if(index < massiv.Length) {
+            Console.Write(", ");
+        }
+    }
+    Console.Write(" ]");
+    Console.WriteLine();
+}
+
+
+int[] array = new int[8];
+
+AllArray(array);
+ArrayWrite(array);
